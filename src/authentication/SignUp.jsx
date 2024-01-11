654e6ef0e9056fd709c85d888086ab16256a6a30
Fredrick Kabiru <50455667+fredie7385@users.noreply.github.com>
2024-01-11 10:39:20 +0300

    feat: sign up form

---
+++ src/authentication/SignUp.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { Box, Button, Modal, TextField, Typography } from "@mui/material";
+import { GoogleAuthProvider, FacebookAuthProvider } from "firebase/auth"; 
+
+export default function SignUp() {
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <>
+      <Button onClick={handleOpen}>Sign Up</Button>
+      <Modal open={open} onClose={handleClose}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
+          <Box
+            component="form"
+            sx={{
+              //   width: { xs: "90%", sm: "500px" },
+              margin: "0 auto",
+              padding: "20px",
+              border: "1px solid #ccc",
+              borderRadius: "10px",
+            }}
+          >
+            <Typography variant="h5" align="center" marginBottom={3}>
+              Sign Up
+            </Typography>
+            <TextField fullWidth label="Name" margin="normal" />
+            <TextField fullWidth label="Email" type="email" margin="normal" />
+            <TextField
+              fullWidth
+              label="Password"
+              type="password"
+              margin="normal"
+            />
+            <Button
+              variant="contained"
+              type="submit"
+              fullWidth
+              onClick={handleClose}
+            >
+              Sign Up
+            </Button>
+          </Box>          
+        </Box>
+      </Modal>
+    </>
+  );
+}
