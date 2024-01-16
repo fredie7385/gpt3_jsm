eeff2293fe0f5a95801a8bf062f304dd749e2d45
Fredrick Kabiru <50455667+fredie7385@users.noreply.github.com>
2024-01-16 15:02:41 +0300

    feat: features  file upload.

--- src/container/features/Features.jsx
+++ src/container/features/Features.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
-import './Features.css';
-export default function Features() {
-  return (
-    <div>Features</div>
-  )
-}
+import React from "react";
+import Feature from "../../components/feature/Feature";
+import "./Features.css";
+import featuresData from "./../../assets/data/features.json";
+
+const Features = () => (
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">
+        The Future is Now and You Just Need to Realize It. Step into Future
+        Today. & Make it Happen.
+      </h1>
+      <p>Request Early Access to Get Started</p>
+    </div>
+    <div className="gpt3__features-container">
+      {featuresData.map((item, index) => (
+        <Feature title={item.title} text={item.text} key={index} />
+      ))}
+    </div>
+  </div>
+);
+
+export default Features;
