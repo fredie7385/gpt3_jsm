07ec421fe4ddf8b9dcbcc91189de90e3a747dfb7
Fredrick Kabiru <50455667+fredie7385@users.noreply.github.com>
2024-01-16 11:04:29 +0300

    feat: whatgpt3 upload.

--- src/container/whatGPT3/WhatGpt3.jsx
+++ src/container/whatGPT3/WhatGpt3.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
 import './WhatGpt3.css';
+import Feature from './../../components/feature/Feature.jsx';
 export default function WhatGpt3() {
   return (
-    <div>WhatGpt3</div>
+    <div className='gpt3__whatgpt3 section__margin' id='whgpt3'>
+      <div className="gpt3__whatgpt3-feature">
+        <Feature/>
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">The Possibilities are beyond your Imaginations</h1>
+        <p>Explore the library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        <Feature/>
+        <Feature/>
+        <Feature/>
+      </div>
+    </div>
   )
 }
